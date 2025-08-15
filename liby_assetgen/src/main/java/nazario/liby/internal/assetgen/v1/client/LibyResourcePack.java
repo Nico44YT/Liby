5c80dbab861051c6007600b6ea37cec443f097d3
Nico44YT <Nico_44-YouTube@gmx.de>
2025-08-15 14:58:55 +0200

    Changed how the runtime resource pack gets added.

--- liby_assetgen/src/main/java/nazario/liby/internal/assetgen/v1/client/LibyResourcePack.java
+++ liby_assetgen/src/main/java/nazario/liby/internal/assetgen/v1/client/LibyResourcePack.java
@@ -14,9 +14,10 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
+import java.util.function.Consumer;
 
 @ApiStatus.Internal
-public class LibyResourcePack implements ResourcePack {
+public class LibyResourcePack implements ResourcePack, ResourcePackProvider {
     public final String name = "liby_runtime_assets";
     public final ResourcePackProfile profile;
     public final PackResourceMetadata metadata;
@@ -86,5 +87,10 @@ public class LibyResourcePack implements ResourcePack {
     @Override
     public void close() {
 
+    }
+
+    @Override
+    public void register(Consumer<ResourcePackProfile> profileAdder) {
+        profileAdder.accept(this.profile);
     }
 }
