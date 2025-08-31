00be1eb311d60a84327695fb7009d888ecf5d663
Nico44YT <Nico_44-YouTube@gmx.de>
2025-08-31 20:08:04 +0200

    Added default dimensions

--- liby_world/src/main/java/nazario/liby/api/world/v1/entity/LibyBoat.java
+++ liby_world/src/main/java/nazario/liby/api/world/v1/entity/LibyBoat.java
@@ -1,5 +1,8 @@
 package nazario.liby.api.world.v1.entity;
 
+import net.minecraft.entity.EntityDimensions;
+
 public interface LibyBoat {
+    public static final EntityDimensions BOAT_DIMENSIONS = EntityDimensions.fixed(1.375F, 0.5625F);
     LibyBoatEntity.LibyBoatType getBoatVariant();
 }
